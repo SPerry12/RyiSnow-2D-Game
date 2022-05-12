c56cf0eb29ff14163b9ac009fd03db2ad4492d10
Shane Perry <perrysm98@gmail.com>
2022-05-12 17:43:33 -0500

    Completed video 5 - rendered world and camera

--- My2DGame/src/entity/Player.java
+++ My2DGame/src/entity/Player.java
@@ -14,11 +14,17 @@ public class Player extends Entity{
 	
 	GamePanel gp;
 	KeyHandler keyH;
+	public final int screenX;
+	public final int screenY;
 	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		
 		this.gp = gp;
 		this.keyH = keyH;
+		
+		screenX = gp.screenWidth/2 - (gp.tileSize/2);
+		screenY = gp.screenHeight/2 - (gp.tileSize/2);
+		
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -26,8 +32,8 @@ public class Player extends Entity{
 	
 	public void setDefaultValues() {
 		
-		x = 100;
-		y = 100;
+		worldX = gp.tileSize * 23;
+		worldY = gp.tileSize * 21;
 		speed = 4;
 		direction = "down";
 	}
@@ -56,19 +62,19 @@ public class Player extends Entity{
 				keyH.leftPressed == true || keyH.rightPressed == true) {
 			if(keyH.upPressed == true) {
 				direction = "up";
-				y -= speed;
+				worldY -= speed;
 			}
 			else if(keyH.downPressed == true) {
 				direction = "down";
-				y += speed;
+				worldY += speed;
 			}
 			else if(keyH.leftPressed == true) {
 				direction = "left";
-				x -= speed;
+				worldX -= speed;
 			}
 			else if(keyH.rightPressed == true) {
 				direction = "right";
-				x += speed;
+				worldX += speed;
 			}
 			
 			spriteCounter++;
@@ -125,7 +131,7 @@ public class Player extends Entity{
 			break;
 		}
 		
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 		
 		
 	}
